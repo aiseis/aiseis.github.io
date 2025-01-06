136da7f7e8d1657b6de2b41417acd3a36dd350e4
aiseis <aiseis@users.noreply.github.com>
2025-01-06 22:09:22 +0000

    deploy: 9ed34974a8b4200080fb75701dc200ad48e8b674

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./assets/css/styles.css"], // Paths to your HTML and CSS files
+    theme: {
+      extend: {
+        colors: {
+          custom: "#44546a",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
